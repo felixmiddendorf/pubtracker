8a46352da0391599d84f56453c3e87a92720613e
Felix <felix@felixmiddendorf.eu>
2009-04-29 00:20:58 +0100

    less text. smaller text. BETTER TEXT

--- templates/menu.php
+++ templates/menu.php
@@ -3,7 +3,7 @@
 <ol>
   <li><a accesskey="1" href="?action=friend&amp;<?= url_token() ?>">Find a friend</a></li>
   <li><a accesskey="2" href="?action=pub&amp;<?= url_token() ?>">Go to a pub</a></li>
-  <li><a accesskey="3" href="?action=status&amp;<?= url_token() ?>">Update your status</a></li>
+  <li><a accesskey="3" href="?action=status&amp;<?= url_token() ?>">Update status</a></li>
   <li><a accesskey="4" href="?action=logout">Logout</a></li>
 </ol>
 <? require TEMPLATE_DIR.'layout_foot.php'; ?>
