668a68c90ad2da7d2f056b9f4ebadef85951a6af
Felix <felix@felixmiddendorf.eu>
2009-04-29 01:06:12 +0100

    stupid bug. switched content-types

--- templates/layout_head.php
+++ templates/layout_head.php
@@ -1,17 +1,15 @@
 <?
 if($mobile){
-	header('Content-Type: application/xhtml+xml; charset=utf-8');
+	header('Content-Type: application/vnd.wap.xhtml+xml; charset=utf-8');
 ?>
 <?= '<?xml version="1.0" encoding="utf-8"?>'."\n"; ?>
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <?
 }else{
-	header('Content-Type: application/vnd.wap.xhtml+xml; charset=utf-8');
+	header('Content-Type: application/xhtml+xml; charset=utf-8');
 ?>
 <?= '<?xml version="1.0" encoding="utf-8"?>'."\n"; ?>
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <? } ?>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
