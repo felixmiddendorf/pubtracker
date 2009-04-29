70c139619e50a450569ad7e28dbe68b7b953ac0d
Felix <felix@felixmiddendorf.eu>
2009-04-29 01:30:09 +0100

    deleted lang attribute

--- templates/layout_head.php
+++ templates/layout_head.php
@@ -11,7 +11,7 @@ if($mobile){
 <?= '<?xml version="1.0" encoding="utf-8"?>'."\n"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <? } ?>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
   <title><?= ($current_user)?$current_user['name']:'PubTracker'; ?></title>
   <link rel="stylesheet" type="text/css" href="./style.php" media="all" />
