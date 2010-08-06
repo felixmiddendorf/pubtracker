ead97408bca3b6e2ee5d6ac619adbb72d0078236
Felix <felix@felixmiddendorf.eu>
2010-08-06 20:59:41 +0200

    Build script fixed

--- doc/build_pdf.sh
+++ doc/build_pdf.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+# clean-up
+rm pubtracker.aux pubtracker.bbl pubtracker.toc pubtracker.log pubtracker.blg
+# build
 pdflatex pubtracker.tex
 pdflatex pubtracker.tex
-bibtex pubtracker.tex
+bibtex pubtracker
 pdflatex pubtracker.tex
 pdflatex pubtracker.tex
 pdflatex pubtracker.tex
